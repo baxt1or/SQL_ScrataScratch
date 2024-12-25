7f230aadf6b5f3a8f57f19e4ae8897166e3a747b
baxt1or <230035@centralasian.com>
2024-12-25 14:36:52 +0500

    Solutions

---
+++ 10355. Employees With Same Birth Month/solution.sql
@@ -0,0 +1,19 @@
+WITH months AS (SELECT
+DISTINCT birth_month AS month FROM employee_list),
+
+report AS (SELECT
+profession, 
+birth_month,
+COUNT(*) AS total_employees
+FROM employee_list a
+WHERE birth_month != 0
+GROUP BY 1, 2)
+
+
+SELECT
+r.profession, 
+m.month, 
+COALESCE(r.total_employees, 0) AS total_employees
+FROM months m
+LEFT JOIN report r ON r.birth_month = m.month
+ORDER BY 2
